e58e59406c12a86f332ae35114b7f6762871aad6
900apm <xiaoge.deng@hotmail.com>
2020-02-27 20:02:38 -0500

    updated portfolio, fixed bug with h2 on top-bar

--- javascript/script.js
+++ javascript/script.js
@@ -138,6 +138,8 @@ let aboutHtml = (
         '<a href="https://jquery.com/" target="_blank"><svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>jQuery</title><path d="M1.534 5.874c-2.123 3.05-1.86 7.017-.237 10.256.037.079.078.154.118.229.023.052.049.1.077.149.013.028.031.057.047.083.026.052.054.102.081.152l.157.265c.029.049.057.097.09.146.055.094.12.187.177.281.026.039.05.078.079.117a6.36 6.36 0 0 0 .31.444c.078.107.156.211.24.315.027.038.057.076.085.114l.221.269c.027.031.054.067.083.099.098.118.202.233.306.349 0 .002.003.004.005.007a3.13 3.13 0 0 0 .424.44c.08.082.16.164.245.244l.101.097c.111.104.222.208.339.308.002 0 .003.002.005.003l.057.05c.102.089.205.178.31.26l.125.105c.085.068.173.133.26.2l.136.104c.093.07.192.139.287.207.035.025.07.05.106.073l.029.023.281.185.12.08c.147.094.293.183.438.271.042.021.084.044.123.068.108.062.22.125.329.183.06.034.122.063.184.094.075.042.153.083.233.125a.324.324 0 0 1 .056.023c.033.015.064.031.096.047.12.06.245.117.375.174.024.01.05.02.076.034.144.063.288.123.437.182.034.01.07.027.105.04.135.051.274.103.411.152l.05.018c.153.052.304.102.459.15.036.01.073.023.111.033.159.048.313.105.473.136 10.26 1.87 13.242-6.169 13.242-6.169-2.505 3.262-6.95 4.122-11.16 3.165-.156-.036-.312-.086-.469-.132a13.522 13.522 0 0 1-.567-.181l-.062-.024c-.136-.046-.267-.097-.4-.148a1.613 1.613 0 0 0-.11-.041c-.147-.059-.29-.12-.432-.183-.031-.01-.057-.024-.088-.036a23.41 23.41 0 0 1-.361-.17c-.037-.016-.07-.033-.106-.052-.094-.044-.188-.094-.28-.142a3.942 3.942 0 0 1-.187-.096c-.113-.06-.226-.125-.339-.187-.034-.024-.073-.044-.112-.066a15.902 15.902 0 0 1-.438-.269 2.104 2.104 0 0 1-.118-.079 6.002 6.002 0 0 1-.312-.206c-.035-.023-.067-.048-.103-.073a9.541 9.541 0 0 1-.294-.212c-.042-.034-.087-.066-.132-.1-.088-.069-.177-.135-.265-.208l-.118-.094a10.58 10.58 0 0 1-.334-.281.258.258 0 0 0-.037-.03l-.347-.316-.1-.094c-.082-.083-.166-.163-.25-.245l-.097-.1a9.07 9.07 0 0 1-.309-.323l-.015-.016c-.106-.116-.209-.234-.313-.354-.027-.031-.052-.064-.08-.097l-.226-.277a21.248 21.248 0 0 1-.34-.448C2.16 11.786 1.315 7.386 3.184 3.777"/><path d="M8.121 3.305c-1.539 2.209-1.452 5.163-.254 7.499a9.1 9.1 0 0 0 .677 1.132c.23.33.484.72.792.986.107.122.223.24.344.359l.09.09c.114.11.231.218.35.325l.015.013a9.855 9.855 0 0 0 .414.342c.034.023.063.05.096.073.14.108.281.212.427.315l.015.009c.062.045.128.086.198.13.028.018.06.042.09.06.106.068.21.132.317.197.017.007.032.016.048.023.09.055.188.108.282.157.033.02.065.035.099.054.067.033.133.068.197.102l.032.014c.135.066.273.128.408.19.034.013.063.024.092.038.111.048.224.094.335.137.05.017.097.037.144.052.102.038.209.073.31.108l.14.045c.146.045.294.104.448.129 7.92 1.313 9.754-4.787 9.754-4.787-1.651 2.376-4.846 3.508-8.251 2.624a8.03 8.03 0 0 1-.448-.13c-.048-.013-.09-.028-.136-.042-.104-.036-.211-.071-.312-.109l-.144-.054c-.112-.045-.226-.087-.335-.135-.034-.015-.065-.025-.091-.04-.14-.063-.281-.125-.417-.192l-.206-.107-.119-.06c-.092-.048-.177-.098-.265-.15a.62.62 0 0 1-.062-.034c-.106-.066-.216-.13-.317-.198-.034-.019-.065-.042-.097-.062l-.208-.136c-.144-.1-.285-.208-.427-.312-.032-.029-.063-.053-.094-.079-1.497-1.177-2.678-2.786-3.238-4.608-.59-1.894-.46-4.018.559-5.742"/><path d="M13.66 1.384c-.908 1.332-.995 2.986-.37 4.455.664 1.56 2.022 2.785 3.604 3.365.065.025.128.046.195.07l.088.027c.092.029.185.063.28.084 4.376.844 5.56-2.247 5.879-2.701-1.042 1.496-2.789 1.855-4.932 1.334a4.844 4.844 0 0 1-.516-.16 6.344 6.344 0 0 1-.617-.254 6.521 6.521 0 0 1-1.08-.66c-1.92-1.454-3.109-4.23-1.857-6.491"/></svg></a>'+
         '<a href="https://reactjs.org/" title="ReactJS" target="_blank"><i class="fab fa-react"></i></a>' +
         '<a href="https://firebase.google.com/" title="Firebase" target="_blank"><svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Firebase </title><path d="M3.89 15.673L6.255.461A.542.542 0 0 1 7.27.289L9.813 5.06 3.89 15.673zm16.795 3.691L18.433 5.365a.543.543 0 0 0-.918-.295l-14.2 14.294 7.857 4.428a1.62 1.62 0 0 0 1.587 0l7.926-4.428zM14.3 7.148l-1.82-3.482a.542.542 0 0 0-.96 0L3.53 17.984 14.3 7.148z"/></svg></a>'+
+        '<a href="https://www.php.net/" title="PHP" target="_blank"><i class="fab fa-php"></i></a>'+
+        '<a href="https://www.adobe.com/products/illustrator.html" title="Adobe Illustrator" target="_blank" ><i class="fab fa-adobe"></i></a>'+
       '</div>' +
 
     '</div>'+
@@ -174,7 +176,7 @@ let portfolioHtml = (
     '<div class="top-bar">'+
         '<button class="enlarge">☐</button>'+
         '<button class="close">×</button>'+
-        '<h2>portfolio</h2>'+
+        '<h2>Portfolio</h2>'+
         '<div class="bar1"></div>'+
         '<div class="bar2"></div>'+
         '<div class="bar3"></div>'+
@@ -196,6 +198,18 @@ let portfolioHtml = (
     //   '<a href="https://900apm.github.io/xiaoge_deng_project1/index.html" target="_blank">External Link</a>'+
     //   '<a href="https://github.com/900apm/xiaoge_deng_project1" target="_blank">GitHub</a>' +
     // '</div>'+
+
+    '<div class="icon-portfolio">'+
+      '<i class="far fa-file-image"></i>'+
+      '<p>shutter.psd</p>'+
+    '</div>'+
+
+    '<p>PSD conversion of a single page responsive website. Created exclusively with only floats and clearfix for positioning.</p>'+
+
+    '<div class="links">'+
+      '<a href="https://900apm.github.io/xiaoge_deng_project1/" target="_blank">External Link</a>'+
+      '<a href="https://github.com/900apm/xiaoge_deng_project1" target="_blank">GitHub</a>' +
+    '</div>'+
   
     '<div class="icon-portfolio">'+
       '<i class="far fa-file-image"></i>'+
@@ -275,7 +289,10 @@ let freelanceHtml = (
     '<div class="freelance-wrapper">' +
       '<a class="freelance-link" target="_blank" href="https://www.sparkcapture.com">sparkcapture.com</a>'+
     '</div >'+
-    '<p>Designed and developed a multipage responsive website featuring a working image gallery for photos as well as popup modals for videos. The site also includes interactive image sliders to compare different packages. Created with SCSS, JavaScript and jQuery</p>'+
+    '<p>Sparkcapture is a media company that specifically deals with creating content for car dealership photos and videos.</p>'+
+    
+    '<p>I have designed and developed a multipage responsive website featuring a working image gallery for photos and popup modals for the videos page. The site also includes interactive image sliders to compare different packages. Created with SCSS, JavaScript and jQuery</p>'+
+
 
     // '<div class="links">'+
     //   '<a href="https://www.sparkcapture.com" target="_blank">External Link</a>'+
@@ -287,7 +304,9 @@ let freelanceHtml = (
       '<a class="freelance-link" target="_blank" href="https://dumbmoney.ca">dumbmoney.ca</a>'+
     '</div >'+
 
-    '<p>Designed and developed a custom wordpress theme specifically for dumbmoney.ca. Created with PHP, SCSS, JavaScript and jQuery. Users are able to register and purchase web courses through learndash and woocommerce and returning users are able to login.</p>' +
+    '<p>Dumbmoney.ca is a website to help Mitchell Huynh expand his online presence and also help sell his book and web course.</p>' +
+
+    "<p>I have designed and developed a custom wordpress theme for dumbmoney.ca, users are able to register and purchase web courses through learndash and woocommerce, returning users are able to login. Created with PHP, SCSS, JavaScript and jQuery. Also includes a video testimonial from Kevin O'Leary!  </p>" +
 
 
     // '<div class="links">'+
@@ -905,7 +924,7 @@ $(".portfolio").on("click", function () {
     $(".portfolio-window")
       .draggable({
         containment: "body",
-        cancel: "h2, p, a",
+        cancel: ".wrapper",
         start: function () {
           $('iframe').css('pointer-events', 'none');
         },
@@ -962,68 +981,7 @@ $(".portfolio").on("click", function () {
 });
 
 // newportfolio window
-  $(".newportfolio").on("click", function () {
-  let clickCounter = 0;
-    if ($('.newportfolio-window').length === 0) {
-      $("main").append(newportfolioHtml);
-      $(".newportfolio-window")
-      .draggable({
-        containment: "body",
-        cancel: "h2, p, a",
-        start: function () {
-          $('iframe').css('pointer-events', 'none');
-        },
-        stop: function () {
-          $('iframe').css('pointer-events', 'auto');
-        }
-      })
-      .resizable({
-        containment: "body",
-        minWidth: 400,
-        minHeight: 550,
-        start: function () {
-          $('iframe').css('pointer-events', 'none');
-        },
-        stop: function () {
-          $('iframe').css('pointer-events', 'auto');
-        }
-      })
-      .on("click", function () {
-        $(this).addClass('top').removeClass('bottom');
-        $(this).siblings().removeClass('top').addClass('bottom');
-      })
-  }
 
-  // closes freelance-window button
-    $(".newportfolio-window .close")
-    .on("click", function () {
-      $(this).parents(".newportfolio-window").remove()
-    });
-  
-  // resize portfolio-window button
-    $(".newportfolio-window .enlarge")
-    .on("click", function () {
-      clickCounter += 1;
-      if (clickCounter % 2 != 0) {
-        // console.log("I am big");
-        $(this).parents(".newportfolio-window").animate({
-          top: "0",
-          left: "0",
-          height: "100%",
-          width: "100%"
-        });
-      }
-      else {
-        // console.log("I am small");
-        $(this).parents(".newportfolio-window").animate({
-          top: "5%",
-          left: $(this).parent().width() / 2 - 400 / 2,
-          height: "550",
-          width: "400"
-        });
-      }
-    });
-});
 
 // freelance window
   $(".freelance").on("click", function () {
@@ -1033,7 +991,7 @@ $(".portfolio").on("click", function () {
       $(".freelance-window")
       .draggable({
         containment: "body",
-        cancel: "h2, p, a",
+        cancel: ".wrapper",
         start: function () {
           $('iframe').css('pointer-events', 'none');
         },
@@ -1081,9 +1039,9 @@ $(".portfolio").on("click", function () {
         // console.log("I am small");
         $(this).parents(".freelance-window").animate({
           top: "5%",
-          left: $(this).parent().width() / 2 - 400 / 2,
+          left: $(this).parent().width() / 2 - 420 / 2,
           height: "550",
-          width: "400"
+          width: "420"
         });
       }
     });
